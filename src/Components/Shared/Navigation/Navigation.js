f86e68072be57c7dad5e90bcba955ad9245cc5e8
ArifulIslam99 <afridi2000rampura@gmail.com>
2022-01-01 13:23:47 +0600

    first commit

---
+++ src/Components/Shared/Navigation/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+const Navigation = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand as={Link} to='/'> Jewellery Shop </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link as={Link} to='/home'>Home</Nav.Link>
+      <Nav.Link as={Link} to='/shop'>Shop</Nav.Link>
+      
+    </Nav>
+    <Nav>
+      <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+      
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+        </div>
+    );
+};
+
+export default Navigation;
