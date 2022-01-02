70bb1f8a8acc4a4b700945a877c3a1c0c350b507
unknown <mistymoni5050@gmail.comgit config --global user.name sangita-das>
2022-01-02 12:08:50 +0600

    about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import About from './Components/Home/About/About';
 import Navigation from './Components/Shared/Navigation/Navigation';
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
       <BrowserRouter>
       <Navigation></Navigation>
           <Switch>
-
+          <Route path="/about">
+              <About></About>
+            </Route>
           </Switch>
       </BrowserRouter>
     </div>
