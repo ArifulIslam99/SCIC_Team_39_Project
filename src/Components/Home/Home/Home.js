6cb69153c310c81b5ee6990a088477e9caa1918c
ArifulIslam99 <afridi2000rampura@gmail.com>
2022-01-03 18:28:39 +0600

    a

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import Footer from '../../Shared/Footer/Footer';
-import About from '../About/About';
 
 const Home = () => {
   return (
     <div>
-    
 
-      <br />
-<About></About>
-      <Footer></Footer>
     </div>
   );
 };
