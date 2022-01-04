cf60caa0906d1c8b44ae7f1c299be4402cea4a65
ArifulIslam99 <afridi2000rampura@gmail.com>
2022-01-04 19:39:23 +0600

    product api

--- src/Components/Shop/DisplayProducts/DisplayProducts.js
+++ src/Components/Shop/DisplayProducts/DisplayProducts.js
@@ -6,10 +6,13 @@ const DisplayProducts = (props) => {
     return (
         <div>
            <div className="col">
-                <div className="card h-100 shadow-sm"> <img src={image} className="card-img-top" alt="..." />
+                <div className="card h-100 shadow-sm p-3"> <img 
+                
+                 style={{width:'400px', height:'400px'}}
+                src={image} className="card-img-top img-fluid " alt="..." />
                     <div className="card-body">
                         <div className="clearfix mb-3"> <span className="float-start badge rounded-pill bg-primary">{productName}</span> <span className="float-end text-danger fw-bold price-hp">{price} Tk</span> </div>
-                        <h5 className="card-title">{description}</h5>
+                        <h5 className="card-title"> {description}</h5>
 
                         <div className="text-center my-4">
                             <button className="btn btn-warning">Purchase Now</button>
